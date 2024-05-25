94d873d1ac7b72c17660a10c6f4078d08bb7e7d6
Rishikesh11kr <rishikesh01119@gmail.com>
2024-05-25 17:32:14 +0530

    Sum of given number

---
+++ Day 4/sum.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Prompt the user for input
+echo "Enter the first number:"
+read num1
+
+echo "Enter the second number:"
+read num2
+
+# Calculate the sum
+sum=$((num1 + num2))
+
+# Print the sum
+echo "The sum is: $sum"
